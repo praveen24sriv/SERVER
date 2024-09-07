afc5558c9bf6b9a4170f207dea9043d37f76ba14
praveen24sriv <praveen24.srivastav@gmail.com>
2024-09-07 21:44:56 +0530

    done

--- script.js
+++ script.js
@@ -5,7 +5,12 @@ const port = 3000;
 app.get('/',(req,res)=>{
     res.send("hello praveen")
 });
-
+app.get('/about',(req,res)=>{
+    res.send("u r reading me about section ....")
+});
+app.get('/contact',(req,res)=>{
+    res.send("contact me at ....")
+});
 
 app.listen(port,()=>{
     console.log(`server running on port ${port}`);
